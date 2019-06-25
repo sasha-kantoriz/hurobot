0d7038d1beed5a129d9bbd91a6f4c23fd7c709bb
root <no.money.no.honey5151@gmail.com>
2019-06-25 23:17:39 +0300

    rotate camera by 180 degrees

--- scripts/camera-photo-slack-upload.sh
+++ scripts/camera-photo-slack-upload.sh
@@ -4,7 +4,7 @@ output_directory=/camera
 output_file=`TZ=Europe/Kiev date +%Y-%m-%d.%H-%M-%S`.jpeg
 export TZ=Europt/Kiev
 
-raspistill -t 1000 -rot 180 -o $output_directory/$output_file #-h 420 -w 510
+raspistill -t 1000 -o $output_directory/$output_file #-h 420 -w 510
 
 curl -F "file=@$output_directory/$output_file" -F 'channels=#random' -F "token=$HUBOT_SLACK_TOKEN" https://slack.com/api/files.upload
 
